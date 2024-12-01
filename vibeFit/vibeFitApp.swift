65405765de94c745deb4e5537e495efa4f1a24b4
Rohan Chaudhry <rohan.chaudhry@utexas.edu>
2024-12-01 14:16:03 -0600

    Initial Commit

---
+++ vibeFit/vibeFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibeFitApp.swift
+//  vibeFit
+//
+//  Created by Rohan Chaudhry on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct vibeFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
